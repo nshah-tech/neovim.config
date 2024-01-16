d6e20ddd3c84fe95cc410d3d0c5fd63e3d7bce59
nshah-tech <nshah@nshah.tech>
2024-01-16 17:12:22 +0530

    Base Commit

--- README.md
+++
@@ -1,2 +0,0 @@
-### nshah's init.lua
-Prerequisite: install [ripgrep](https://github.com/BurntSushi/ripgrep).
--- after/plugin/nvim-tree.lua
+++
@@ -1,5 +0,0 @@
-require'nvim-tree'.setup()
-
--- vim.api.nvim_set_keymap('n', '<leader>w', ':NvimTreeFocus <CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-b>', ':NvimTreeToggle <CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<S-b>', ': NvimTreeFindFile <CR>', { noremap = true })
