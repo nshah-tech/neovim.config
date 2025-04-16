ef489a9d19380b30fe237ebabdb93d93c21ea1a2
nshah-tech <nshah@nshah.tech>
2025-04-16 14:45:47 +0530

    Version 8 Test?

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,16 +2,13 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     cmd = "Telescope",
-    enabled = function()
-      return LazyVim.pick.want() == "telescope"
-    end,
     version = false, -- telescope did only one release, so use HEAD for now
     dependencies = {
       {
         "nvim-telescope/telescope-fzf-native.nvim",
-        build = have_make and "make"
+        build = (build_cmd ~= "cmake") and "make"
           or "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-        enabled = have_make or have_cmake,
+        enabled = build_cmd ~= nil,
         config = function(plugin)
           LazyVim.on_load("telescope.nvim", function()
             local ok, err = pcall(require("telescope").load_extension, "fzf")
@@ -38,12 +35,16 @@ return {
       },
       { "<leader>/", LazyVim.pick("live_grep"), desc = "Grep (Root Dir)" },
       { "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
-      { "<leader>fh", LazyVim.pick("auto"), desc = "Find Files (Root Dir)" },
+      { "<leader><space>", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
       -- find
-      { "<leader>fb", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>", desc = "Buffers" },
+      {
+        "<leader>fb",
+        "<cmd>Telescope buffers sort_mru=true sort_lastused=true ignore_current_buffer=true<cr>",
+        desc = "Buffers",
+      },
       { "<leader>fc", LazyVim.pick.config_files(), desc = "Find Config File" },
-      { "<leader>ff", LazyVim.pick("auto"), desc = "Find Files (Root Dir)" },
-      { "<leader>fF", LazyVim.pick("auto", { root = false }), desc = "Find Files (cwd)" },
+      { "<leader>ff", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
+      { "<leader>fF", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
       { "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Find Files (git-files)" },
       { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
       { "<leader>fR", LazyVim.pick("oldfiles", { cwd = vim.uv.cwd() }), desc = "Recent (cwd)" },
@@ -111,6 +112,20 @@ return {
         LazyVim.pick("find_files", { hidden = true, default_text = line })()
       end
 
+      local function find_command()
+        if 1 == vim.fn.executable("rg") then
+          return { "rg", "--files", "--color", "never", "-g", "!.git" }
+        elseif 1 == vim.fn.executable("fd") then
+          return { "fd", "--type", "f", "--color", "never", "-E", ".git" }
+        elseif 1 == vim.fn.executable("fdfind") then
+          return { "fdfind", "--type", "f", "--color", "never", "-E", ".git" }
+        elseif 1 == vim.fn.executable("find") and vim.fn.has("win32") == 0 then
+          return { "find", ".", "-type", "f" }
+        elseif 1 == vim.fn.executable("where") then
+          return { "where", "/r", ".", "*" }
+        end
+      end
+
       return {
         defaults = {
           prompt_prefix = " ",
@@ -142,6 +157,12 @@ return {
             n = {
               ["q"] = actions.close,
             },
+          },
+        },
+        pickers = {
+          find_files = {
+            find_command = find_command,
+            hidden = true,
           },
         },
       }
