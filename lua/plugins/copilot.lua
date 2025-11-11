1c88197722442b611b99651236db8003a2e99fdb
nshah-tech <nshah@nshah.tech>
2025-11-11 17:55:43 +0530

    Works

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -151,7 +151,7 @@ return {
     },
     config = function(_, opts)
       local chat = require("CopilotChat")
-      require("CopilotChat.integrations.cmp").setup()
+      -- require("CopilotChat.integrations.cmp").setup()
 
       vim.api.nvim_create_autocmd("BufEnter", {
         pattern = "copilot-chat",
