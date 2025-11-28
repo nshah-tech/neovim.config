a36c28f01d7dfb8ba5530e597164b3c92e245c57
nshah-tech <nshah@nshah.tech>
2025-11-28 09:27:39 +0530

    Update Cleanup 1

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -3,4 +3,8 @@ return {
   opts = {
     window = { position = "right" },
   },
+  keys = {
+    { "<leader>E", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
+    { "<leader>e", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+  },
 }
